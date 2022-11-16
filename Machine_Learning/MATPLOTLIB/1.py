4de5f0f722aaf43d15c4ad626f07179abd75cd87
Mo7amedAtwan <mo7amed3twan@gmail.com>
2022-11-17 01:05:30 +0200

    1

---
+++ Machine_Learning/MATPLOTLIB/1.py
@@ -0,0 +1,6 @@
+fig,ax=plt.subplots(1, 4, figsize=(12, 3), sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train)
+    ax[i].set_xlabel(X_features[i])
+ax[i].set_ylabel("Price (1000's)")
+plt.show()
