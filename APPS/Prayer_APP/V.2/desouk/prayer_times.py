b5091e00170e1305761ecc8cdecb3e13b8120d4d
Mo7amedAtwan <mo7amed3twan@gmail.com>
2022-11-17 18:23:27 +0200

    1

--- APPS/Prayer_APP/V.2/desouk/prayer_times.py
+++ APPS/Prayer_APP/V.2/desouk/prayer_times.py
@@ -21,13 +21,11 @@ html = requests.get(url).content
 soup = S(html,'html.parser')
 site_json=json.loads(soup.text)
 timings = list(site_json['data']['timings'].values())[0:7]
-f=timings[0]
-d=timings[2]
+fajr=timings[0]
+dhuhr=timings[2]
 asr=timings[3]
-mag=timings[5]
-ish=timings[6]
-
-print(f,d,asr,mag,ish)
+maghrib=timings[5]
+isha=timings[6]
 
 
 #make window
@@ -45,28 +43,21 @@ l1=Label(test,text='',height=1,font=("Arial",10)).pack()
 title=Label(test,text="مواقيت الصلاة فى دسوق",height=2,font=("ِArial",30),relief="raised",bd=10,fg='#E7EAEF',bg='#37745B').pack()
 l2=Label(test,text='',height=1,font=("Arial",10)).pack()
 ##################
-l3= Label(test,text="TIME NOW :",height=1,font=("Arial",20),fg='#02315E')
+l3= Label(test,text="",height=1,font=("Arial",20),fg='#02215E')
 l3.pack()
-
-
-
-
 #current time
 def updateTime():
     currentime = datetime.now()
-    hours = currentime.strftime('%H')
-    minutes = currentime.strftime('%M')
-    seconds = currentime.strftime('%S')
-    mixed = currentime.strftime('%I:%M')
-    l3.configure(text=f"TIME NOW : {mixed}")
+    showTime = currentime.strftime('%H:%M:%S')
+    time = currentime.strftime('%H:%M')
+    l3.configure(text=f"Time Now  {showTime}")
     for i in range(len(timings)):
         timePray = timings[i]
-        timeNow = str(mixed).lstrip('0')
+        timeNow = str(time).lstrip('0')
         if(timeNow == timePray):
-            play('mixkit-doorbell-single-press-333.wav')
+            play('s1.mp3')
         else:
-            print(timePray)
-            print(timeNow)
+            pass
     test.after(1000 ,updateTime)
 
 updateTime()
@@ -74,11 +65,11 @@ updateTime()
 
 ##################
 l4=Label(test,text='',height=1,font=("Arial",10)).pack()
-l5=Label(test,text=obj[0].text,height=1,font=("Arial",40),fg='#02315E').pack()
-l6=Label(test,text=obj[2].text,height=1,font=("Arial",40),fg='#00457E').pack()
-l7=Label(test,text=obj[3].text,height=1,font=("Arial",40),fg='#2F70AF').pack()
-l8=Label(test,text=obj[4].text,height=1,font=("Arial",40),fg='#B9848C').pack()
-l9=Label(test,text=obj[5].text,height=1,font=("Arial",40),fg='#806491').pack()
+l5=Label(test,text=f"Fajr : {fajr}",height=1,font=("Arial",40),fg='#02315E').pack()
+l6=Label(test,text=f"Dhuhr : {dhuhr}",height=1,font=("Arial",40),fg='#00457E').pack()
+l7=Label(test,text=f"Asr : {asr}",height=1,font=("Arial",40),fg='#2F70AF').pack()
+l8=Label(test,text=f"Maghrib : {maghrib}",height=1,font=("Arial",40),fg='#B9848C').pack()
+l9=Label(test,text=f"Isha : {isha}",height=1,font=("Arial",40),fg='#806491').pack()
 l10=Label(test,text='',height=1,font=("Arial",10)).pack()
 exit_button=Button(test,text="QUIT",height=1,font=("ِArial",20),command=test.destroy,relief="raised",bd=10,fg='#E7EAEF',bg='#37745B').pack()
 
